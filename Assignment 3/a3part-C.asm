a549ffacb91f58d130de619eafbe26022989e925
Jiayue <126517091+cronchyjia@users.noreply.github.com>
2023-05-03 19:51:43 -0700

    Add files via upload

---
+++ Assignment 3/a3part-C.asm
@@ -0,0 +1,573 @@
+; a3part-C.asm
+;
+; Part C of assignment #3
+;
+;
+; Student name: Jiayue Wong
+; Student ID: V00------
+; Date of completed work: 2/21/2023
+;
+; **********************************
+; Code provided for Assignment #3
+;
+; Author: Mike Zastre (2022-Nov-05)
+;
+; This skeleton of an assembly-language program is provided to help you 
+; begin with the programming tasks for A#3. As with A#2 and A#1, there are
+; "DO NOT TOUCH" sections. You are *not* to modify the lines within these
+; sections. The only exceptions are for specific changes announced on
+; Brightspace or in written permission from the course instruction.
+; *** Unapproved changes could result in incorrect code execution
+; during assignment evaluation, along with an assignment grade of zero. ***
+;
+
+
+; =============================================
+; ==== BEGINNING OF "DO NOT TOUCH" SECTION ====
+; =============================================
+;
+; In this "DO NOT TOUCH" section are:
+; 
+; (1) assembler direction setting up the interrupt-vector table
+;
+; (2) "includes" for the LCD display
+;
+; (3) some definitions of constants that may be used later in
+;     the program
+;
+; (4) code for initial setup of the Analog-to-Digital Converter
+;     (in the same manner in which it was set up for Lab #4)
+;
+; (5) Code for setting up three timers (timers 1, 3, and 4).
+;
+; After all this initial code, your own solutions's code may start
+;
+
+.cseg
+.org 0
+	jmp reset
+
+; Actual .org details for this an other interrupt vectors can be
+; obtained from main ATmega2560 data sheet
+;
+.org 0x22
+	jmp timer1
+
+; This included for completeness. Because timer3 is used to
+; drive updates of the LCD display, and because LCD routines
+; *cannot* be called from within an interrupt handler, we
+; will need to use a polling loop for timer3.
+;
+; .org 0x40
+;	jmp timer3
+
+.org 0x54
+	jmp timer4
+
+.include "m2560def.inc"
+.include "lcd.asm"
+
+.cseg
+#define CLOCK 16.0e6
+#define DELAY1 0.01
+#define DELAY3 0.1
+#define DELAY4 0.5
+
+#define BUTTON_RIGHT_MASK 0b00000001	
+#define BUTTON_UP_MASK    0b00000010
+#define BUTTON_DOWN_MASK  0b00000100
+#define BUTTON_LEFT_MASK  0b00001000
+
+#define BUTTON_RIGHT_ADC  0x032
+#define BUTTON_UP_ADC     0x0b0   ; was 0x0c3
+#define BUTTON_DOWN_ADC   0x160   ; was 0x17c
+#define BUTTON_LEFT_ADC   0x22b
+#define BUTTON_SELECT_ADC 0x316
+
+.equ PRESCALE_DIV=1024   ; w.r.t. clock, CS[2:0] = 0b101
+
+; TIMER1 is a 16-bit timer. If the Output Compare value is
+; larger than what can be stored in 16 bits, then either
+; the PRESCALE needs to be larger, or the DELAY has to be
+; shorter, or both.
+.equ TOP1=int(0.5+(CLOCK/PRESCALE_DIV*DELAY1))
+.if TOP1>65535
+.error "TOP1 is out of range"
+.endif
+
+; TIMER3 is a 16-bit timer. If the Output Compare value is
+; larger than what can be stored in 16 bits, then either
+; the PRESCALE needs to be larger, or the DELAY has to be
+; shorter, or both.
+.equ TOP3=int(0.5+(CLOCK/PRESCALE_DIV*DELAY3))
+.if TOP3>65535
+.error "TOP3 is out of range"
+.endif
+
+; TIMER4 is a 16-bit timer. If the Output Compare value is
+; larger than what can be stored in 16 bits, then either
+; the PRESCALE needs to be larger, or the DELAY has to be
+; shorter, or both.
+.equ TOP4=int(0.5+(CLOCK/PRESCALE_DIV*DELAY4))
+.if TOP4>65535
+.error "TOP4 is out of range"
+.endif
+
+reset:
+; ***************************************************
+; **** BEGINNING OF FIRST "STUDENT CODE" SECTION ****
+; ***************************************************
+
+; Anything that needs initialization before interrupts
+; start must be placed here.
+; symbonic names for registers
+
+	rcall lcd_init
+	;modiefied from lab 4
+	;symbonic names for registers
+	.def DATAH=r25  ;DATAH:DATAL  store 10 bits data from ADC
+	.def DATAL=r24
+	.def BOUNDARY_H=r1  ;hold high byte value of the threshold for button
+	.def BOUNDARY_L=r0  ;hold low byte value of the threshold for button, r1:r0
+
+	ldi r30, 0
+	sts CURRENT_CHARSET_INDEX, r30
+	ldi r19,0
+
+; ***************************************************
+; ******* END OF FIRST "STUDENT CODE" SECTION *******
+; ***************************************************
+
+; =============================================
+; ====  START OF "DO NOT TOUCH" SECTION    ====
+; =============================================
+
+	; initialize the ADC converter (which is needed
+	; to read buttons on shield). Note that we'll
+	; use the interrupt handler for timer 1 to
+	; read the buttons (i.e., every 10 ms)
+	;
+	ldi temp, (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0)
+	sts ADCSRA, temp
+	ldi temp, (1 << REFS0)
+	sts ADMUX, r16
+
+	; Timer 1 is for sampling the buttons at 10 ms intervals.
+	; We will use an interrupt handler for this timer.
+	ldi r17, high(TOP1)
+	ldi r16, low(TOP1)
+	sts OCR1AH, r17
+	sts OCR1AL, r16
+	clr r16
+	sts TCCR1A, r16
+	ldi r16, (1 << WGM12) | (1 << CS12) | (1 << CS10)
+	sts TCCR1B, r16
+	ldi r16, (1 << OCIE1A)
+	sts TIMSK1, r16
+
+	; Timer 3 is for updating the LCD display. We are
+	; *not* able to call LCD routines from within an 
+	; interrupt handler, so this timer must be used
+	; in a polling loop.
+	ldi r17, high(TOP3)
+	ldi r16, low(TOP3)
+	sts OCR3AH, r17
+	sts OCR3AL, r16
+	clr r16
+	sts TCCR3A, r16
+	ldi r16, (1 << WGM32) | (1 << CS32) | (1 << CS30)
+	sts TCCR3B, r16
+	; Notice that the code for enabling the Timer 3
+	; interrupt is missing at this point.
+
+	; Timer 4 is for updating the contents to be displayed
+	; on the top line of the LCD.
+	ldi r17, high(TOP4)
+	ldi r16, low(TOP4)
+	sts OCR4AH, r17
+	sts OCR4AL, r16
+	clr r16
+	sts TCCR4A, r16
+	ldi r16, (1 << WGM42) | (1 << CS42) | (1 << CS40)
+	sts TCCR4B, r16
+	ldi r16, (1 << OCIE4A)
+	sts TIMSK4, r16
+
+	sei
+
+; =============================================
+; ====    END OF "DO NOT TOUCH" SECTION    ====
+; =============================================
+
+; ****************************************************
+; **** BEGINNING OF SECOND "STUDENT CODE" SECTION ****
+; ****************************************************
+
+start:
+	rjmp timer3
+
+timer1:
+;modiefied from lab 4
+	push DATAL
+	push DATAH
+	push r16
+	in r16, sreg
+	push r16
+	push r23
+	push r22
+
+check_button:
+	lds	r16, ADCSRA
+
+	; bit 6 =1 ADSC (ADC Start Conversion bit), remain 1 if conversion not done
+	; ADSC changed to 0 if conversion is done
+	ori r16, 0x40 ; 0x40 = 0b01000000
+	sts	ADCSRA, r16
+
+	; wait for it to complete, check for bit 6, the ADSC bit
+wait:	
+	lds r16, ADCSRA
+	andi r16, 0x40
+	brne wait
+
+	; read the value, use XH:XL to store the 10-bit result
+	lds DATAL, ADCL
+	lds DATAH, ADCH
+
+	clr r23
+	ldi r22, 1
+
+	ldi r16, low(BUTTON_SELECT_ADC)
+	cp DATAL, r16
+	ldi r16, high(BUTTON_SELECT_ADC)
+	cpc DATAH, r16
+	brsh skip		
+	ldi r23,1
+
+	cp DATAL, BOUNDARY_L
+	cpc DATAH, BOUNDARY_H
+	brsh skip		
+	ldi r23,1
+
+right:
+	ldi r16, low(BUTTON_RIGHT_ADC)
+	cp DATAL, r16
+	ldi r16, high(BUTTON_RIGHT_ADC)
+	cpc DATAH, r16
+	brsh up
+	ldi r22, 5
+	rjmp skip
+
+up:
+	ldi r16, low(BUTTON_UP_ADC)
+	cp DATAL, r16
+	ldi r16, high(BUTTON_UP_ADC)
+	cpc DATAH, r16
+	brsh down
+	ldi r22, 4
+	rjmp skip 
+
+down:
+	ldi r16, low(BUTTON_DOWN_ADC)
+	cp DATAL, r16
+	ldi r16, high(BUTTON_DOWN_ADC)
+	cpc DATAH, r16
+	brsh left
+	ldi r22, 3
+	rjmp skip
+		
+left:
+	ldi r22, 2
+
+
+skip:
+	sts BUTTON_IS_PRESSED, r23
+	sts LAST_BUTTON_PRESSED, r22
+	pop r22
+	pop r23
+	pop r16
+	out sreg, r16
+	pop r16
+	pop DATAH
+	pop DATAL
+	reti
+
+timer3:
+	in temp, TIFR3
+	sbrs temp, OCF3A
+	rjmp timer3
+
+	ldi temp, 1<<OCF3A
+	out TIFR3, temp
+	
+display:
+	ldi r16, 1
+	ldi r17, 15
+	push r16 ;row
+	push r17 ;column
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+	
+	ldi r16, '-'
+	push r16
+	rcall lcd_putchar
+	pop r16
+
+	lds r23, BUTTON_IS_PRESSED
+	cpi r23, 0
+	breq display
+
+which_butt:	
+	push r22
+	push r17
+	push r16
+	lds r22, LAST_BUTTON_PRESSED
+
+	ldi r16, 1
+	ldi r17, 15
+	push r16 ;row
+	push r17 ;column
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+
+	ldi r16, '*'
+	push r16
+	rcall lcd_putchar
+	pop r16
+
+displayR:
+	ldi r16, 1
+	ldi r17, 3
+	push r16 ;row
+	push r17 ;column
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+
+	ldi r16, 'R'
+	ldi r17, 5
+	cpse r22, r17
+	ldi r16, ' '
+	push r16
+	rcall lcd_putchar
+	pop r16
+
+
+displayU:
+	ldi r16, 1
+	ldi r17, 2
+	push r16 ;row
+	push r17 ;column
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+
+	ldi r16, 'U'
+	ldi r17, 4
+	cpse r22, r17
+	ldi r16, ' '
+	push r16
+	rcall lcd_putchar
+	pop r16
+
+
+displayD:	
+	ldi r16, 1
+	ldi r17, 1
+	push r16 ;row
+	push r17 ;column
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+
+	ldi r16, 'D'
+	ldi r17, 3
+	cpse r22, r17
+	ldi r16, ' '
+	push r16
+	rcall lcd_putchar
+	pop r16
+
+displayL:
+	ldi r16, 1
+	ldi r17, 0
+	push r16 ;row
+	push r17 ;column
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+
+	ldi r16, 'L'
+	ldi r17, 2
+	cpse r22, r17
+	ldi r16, ' '
+	push r16
+	rcall lcd_putchar
+	pop r16
+
+top_content:
+	ldi r16, 0
+	ldi r17, 0
+	push r16 ;row
+	push r17 ;column
+	rcall lcd_gotoxy
+	pop r17
+	pop r16
+
+	lds r30, TOP_LINE_CONTENT
+	push r30
+	rcall lcd_putchar
+	pop r30
+	rjmp start
+
+popping:
+	pop r16
+	pop r17
+	pop r22
+	rjmp start
+
+timer4:
+	;modiefied from lab 4
+	push ZL
+	push ZH
+	push r16
+	in r16, sreg
+	push r16
+	push r17
+	push r18
+	push r19
+	push r22 ;checks if top or bottom button
+
+	lds r17, CURRENT_CHARSET_INDEX ;index of charset
+	ldi r19, 0
+	mov r19, r17
+
+	ldi ZH, HIGH(AVAILABLE_CHARSET << 1);grabs availible charset pointer
+	ldi ZL, LOW(AVAILABLE_CHARSET << 1 )
+	lpm r18, Z
+	lds r22, LAST_BUTTON_PRESSED
+
+	cpi r22, 4
+	breq increase
+	cpi r22, 3
+	breq decrease
+	
+is_it_out_of_bounds:
+	cpi r19, 0  ;check to reach if the index has reached 0 again, won't be able to go lower
+	breq timer_done
+	adiw Z, 1
+	lpm r18, Z
+	cpi r18, 0  ;check if the value is the null 0 at the end of the string and if so reset the index to 1 in the charset
+	breq index_reset
+	dec r19
+	rjmp is_it_out_of_bounds
+
+increase:
+	inc r17
+	rjmp is_it_out_of_bounds
+
+decrease:
+	dec r17
+	rjmp is_it_out_of_bounds
+
+index_reset: ;if it reaches 0, resets it to the start of the string
+	ldi r17, 0
+	rjmp timer_done
+
+timer_done:
+	sts CURRENT_CHARSET_INDEX, r17
+	sts TOP_LINE_CONTENT, r18
+	pop r22
+	pop r19
+	pop r18
+	pop r17
+		
+	pop r16
+	out sreg, r16
+	pop r16
+	pop ZH
+	pop ZL
+	reti
+
+; ****************************************************
+; ******* END OF SECOND "STUDENT CODE" SECTION *******
+; ****************************************************
+
+
+; =============================================
+; ==== BEGINNING OF "DO NOT TOUCH" SECTION ====
+; =============================================
+
+; r17:r16 -- word 1
+; r19:r18 -- word 2
+; word 1 < word 2? return -1 in r25
+; word 1 > word 2? return 1 in r25
+; word 1 == word 2? return 0 in r25
+;
+compare_words:
+	; if high bytes are different, look at lower bytes
+	cp r17, r19
+	breq compare_words_lower_byte
+
+	; since high bytes are different, use these to
+	; determine result
+	;
+	; if C is set from previous cp, it means r17 < r19
+	; 
+	; preload r25 with 1 with the assume r17 > r19
+	ldi r25, 1
+	brcs compare_words_is_less_than
+	rjmp compare_words_exit
+
+compare_words_is_less_than:
+	ldi r25, -1
+	rjmp compare_words_exit
+
+compare_words_lower_byte:
+	clr r25
+	cp r16, r18
+	breq compare_words_exit
+
+	ldi r25, 1
+	brcs compare_words_is_less_than  ; re-use what we already wrote...
+
+compare_words_exit:
+	ret
+
+.cseg
+AVAILABLE_CHARSET: .db "0123456789abcdef_", 0
+
+
+.dseg
+
+BUTTON_IS_PRESSED: .byte 1			; updated by timer1 interrupt, used by LCD update loop
+LAST_BUTTON_PRESSED: .byte 1        ; updated by timer1 interrupt, used by LCD update loop
+
+TOP_LINE_CONTENT: .byte 16			; updated by timer4 interrupt, used by LCD update loop
+CURRENT_CHARSET_INDEX: .byte 16		; updated by timer4 interrupt, used by LCD update loop
+CURRENT_CHAR_INDEX: .byte 1			; ; updated by timer4 interrupt, used by LCD update loop
+
+
+; =============================================
+; ======= END OF "DO NOT TOUCH" SECTION =======
+; =============================================
+
+
+; ***************************************************
+; **** BEGINNING OF THIRD "STUDENT CODE" SECTION ****
+; ***************************************************
+
+.dseg
+
+; If you should need additional memory for storage of state,
+; then place it within the section. However, the items here
+; must not be simply a way to replace or ignore the memory
+; locations provided up above.
+
+
+; ***************************************************
+; ******* END OF THIRD "STUDENT CODE" SECTION *******
+; ***************************************************
+
